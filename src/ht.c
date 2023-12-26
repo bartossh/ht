410a08eaef16a279af0d6a41c85f092d6c76a213
Bartossh <lenart.consulting@gmail.com>
2023-12-26 11:53:36 +0100

    3: Provided SDBM and Loss Loss hash functions and microbenchmark tests.

--- src/ht.c
+++ src/ht.c
@@ -2,11 +2,33 @@
 #include <stdlib.h>
 
 unsigned long
-HT_hash(unsigned char *str) {
+HT_HashDJB2(unsigned char *str) {
     unsigned long hash = 5381;
     int c;
     while ((c = *str++))
         hash = ((hash << 5) + hash) + c;
     
+    return hash;
+}
+
+unsigned long
+HT_HashSDBM(unsigned char *str) {
+    unsigned long hash = 0;
+    int c;
+
+    while ((c = *str++))
+        hash = c + (hash << 6) + (hash << 16) - hash;
+
+    return hash;
+}
+
+unsigned long
+HT_HashLL(unsigned char *str) {
+    unsigned int hash = 0;
+    int c;
+
+    while ((c = *str++))
+        hash += c;
+
     return hash;
 }
